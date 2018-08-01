8c42fcdcaaa45505b6e2560e52579c526ea66bc4
approxua <kotenko1995@gmial.com>
2018-08-01 12:58:13 +0300

    add main product and first slider

---
+++ dist/js/common.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(function () {
+    $('.product-slider1').slick({
+
+    })
+};
---
+++ src/js/common.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(document).ready(function(){
+    $('.product-slider1').slick({
+        slidesToShow: 5,
+        prevArrow: '<i class="icon-left-open arrow">',
+        nextArrow: '<i class="icon-right-open arrow">'
+    });
+});
