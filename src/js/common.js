a6cf18a596927853081921b086677aba58e70e8e
approxua <kotenko1995@gmial.com>
2018-08-09 09:52:51 +0300

    add tabs

--- src/js/common.js
+++ src/js/common.js
@@ -9,6 +9,11 @@ $(function () {
         prevArrow: '<i class="icon-left-open arrow">',
         nextArrow: '<i class="icon-right-open arrow">'
     });
+    $('.article-slider').slick({
+        slidesToShow: 1,
+        prevArrow: '<i class="icon-left-open arrow">',
+        nextArrow: '<i class="icon-right-open arrow">'
+    });
     $('.character-body').readmore({
         speed: 75,
         moreLink: '<a href="#" class="character-other"><i class="icon-down-open"></i>Показать все характеристики</a>',
@@ -25,6 +30,36 @@ $(function () {
         speed: 75,
         moreLink: '<a href="#" class="character-other"><i class="icon-down-open"></i>Показать все отзывы</a>',
         lessLink: '<a href="#" class="character-other"><i class="icon-up-open"></i>Показать все отзывы</a>',
-        collapsedHeight: 823
+        collapsedHeight: 766
+    });
+    $("#desc").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top - 60;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+    $('.feedback-buttonns div').on('click', 'a', function (e) {
+        e.preventDefault();
+        $(this)
+            .addClass('active')
+            .siblings()
+            .removeClass('active')
+            .closest('div.feedback-wrapper')
+            .find('div.feedback-body')
+            .removeClass('active')
+            .readmore('destroy')
+            .eq($(this).index())
+            .addClass('active')
+            .readmore({
+                speed: 75,
+                moreLink: '<a href="#" class="character-other"><i class="icon-down-open"></i>Показать все отзывы</a>',
+                lessLink: '<a href="#" class="character-other"><i class="icon-up-open"></i>Показать все отзывы</a>',
+                collapsedHeight: 766
+            });
+    });
+    $('.also-buy').on('click', 'a', function (e) {
+        e.preventDefault();
     });
 });
+
